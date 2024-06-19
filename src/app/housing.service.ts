6ad8320a1f31e8bb326aad62d5de43c70f16c053
mattorga <mattheworga85@gmail.com>
2024-06-19 09:17:03 +0800

    Added forms

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -119,4 +119,8 @@ export class HousingService {
     return this.housingLocationList.find(
       housingLocation => housingLocation.id === id);
   }
+
+  submitApplication(firstName: string, lastName: string, email: string){
+    console.log(firstName, lastName, email);
+  }
 }
